5081acf7915cdcaeac5f95c255b2a9a6d309d434
João <ut7vitor60@gmail.com>
2024-07-16 18:02:15 -0300

    Adição do código js

---
+++ script.js
@@ -0,0 +1,10 @@
+let texto = document.querySelector('.input-text')
+let botao = document.querySelector('.criptografar')
+
+function mostra(){
+console.log(texto.value)
+}
+
+botao.addEventListener('click',mostra)
+
+console.log('nada')
