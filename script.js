c79b2fcef93e12c8afe5ad756aeee7941e0582a9
João <ut7vitor60@gmail.com>
2024-07-18 16:37:44 -0300

    Funções finalizadas

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-let texto = document.querySelector('.input-text')
-let botao__criptografar = document.querySelector('.criptografar')
-let botao__descriptografar = document.querySelector('.descriptografar')
+let texto__entrada = document.querySelector('.input-text');
+let botao__criptografar = document.querySelector('.criptografar');
+let botao__descriptografar = document.querySelector('.descriptografar');
+let resultado__saida = document.querySelector('.resposta');
 
-function criptografar(){
-    texto = texto.value
-    let lista = texto.split("");
-    lista.forEach(function)
-    texto_novo = texto.replace('e','enter')
-    console.log(lista)
+// Função para criptografar o texto
+function criptografar() {
+    let texto__conteudo = texto__entrada.value;
+    let textoCriptografado = texto__conteudo
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+    resultado__saida.textContent = textoCriptografado;
 }
 
-botao__criptografar.addEventListener('click',criptografar)
+// Função para descriptografar o texto
+function descriptografar() {
+    let texto__conteudo = texto__entrada.value;
+    let textoDescriptografado = texto__conteudo
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+    resultado__saida.textContent = textoDescriptografado;
+}
 
-console.log('nada')
+botao__criptografar.addEventListener('click', criptografar);
+botao__descriptografar.addEventListener('click', descriptografar);
